63f4bde70d6a7251b33b00750eac93d760ac01af
Raphael Oliveira <raphaelbrf@gmail.com>
2023-06-06 12:36:56 -0300

    .

--- airflow_pipeline/dags/twitter_dag.py
+++ airflow_pipeline/dags/twitter_dag.py
@@ -9,13 +9,12 @@ from os.path import join
 from airflow.utils.dates import days_ago
 from pathlib import Path
 
-TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
-current_date = datetime.now().date().strftime(TIMESTAMP_FORMAT)
+# TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
+# current_date = datetime.now().date().strftime(TIMESTAMP_FORMAT)
 
 with DAG(dag_id = "TwitterDAG", start_date=days_ago(6), schedule_interval="@daily") as dag:
     
     query = "datascience"
-    print(current_date)
 
     twitter_operator = TwitterOperator(
         file_path=join("datalake/twitter_spark/extract_date={{ data_interval_start.strftime('%Y-%m-%d') }}", "{{ ts }}.json"),
