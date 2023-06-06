54ea4286e5d24112cf68ebd6c34acd57e872a22e
Raphael Oliveira <raphaelbrf@gmail.com>
2023-06-06 11:27:31 -0300

    ok

--- airflow_pipeline/dags/twitter_dag.py
+++ airflow_pipeline/dags/twitter_dag.py
@@ -13,11 +13,12 @@ with DAG(dag_id = "TwitterDAG", start_date=days_ago(6), schedule_interval="@dail
     TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
     query = "datascience"
 
-    twitter_operator = TwitterOperator(file_path=join("curso2/datalake/twitter_datascience/extract_date={{ data_interval_start.strftime('%Y-%m-%d') }}",
-                                        "datascience_{{ ds_nodash }}.json"),
-                                        query=query,
-                                        start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
-                                        end_time="{{ data_interval_end.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
-                                        task_id="twitter_datascience")
+    twitter_operator = TwitterOperator(
+        file_path=join("curso2/datalake/twitter_datascience/extract_date={{ data_interval_start.strftime('%Y-%m-%d') }}", "datascience_{{ ds_nodash }}.json"),
+        start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
+        end_time="{{ data_interval_end.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
+        query=query,
+        
+        task_id="twitter_datascience")
 
 twitter_operator
